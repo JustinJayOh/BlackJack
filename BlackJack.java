ede8c887ae85eefa263017a1ac0439c2808b2819
JustinJayOh <41486925+JustinJayOh@users.noreply.github.com>
2019-01-02 16:31:51 -0800

    Add files via upload

---
+++ BlackJack.java
@@ -0,0 +1,189 @@
+import java.util.Scanner;
+
+public class BlackJack {
+
+   public static void main(String[] args) {
+   	
+      Scanner scan = new Scanner(System.in);
+      Player Me = new Player();
+      Player Dealer = new Player();
+      int doubleBet = 0, bet = 0;
+   	
+      int userInput;
+   	
+      // Initalize game
+      System.out.println("Game Start");
+      System.out.println("You Start with $ " + Me.getPocket());
+      System.out.println("Dealing...");
+      Me.resetDeck();
+   	
+      // Loop for Betting and Game 
+      do {
+      
+      // Initialize cards for hand
+         Me.playerHit();
+         Me.playerHit();
+         Dealer.playerHit();
+      
+      // Set bet amount
+         System.out.print("Enter an amount to bet: ");
+         bet = scan.nextInt();
+      
+      // Checks for valid bet amount
+         while (bet > Me.getPocket() || bet <= 0) {
+            System.out.println("Invalid Bet Amount.");
+            System.out.print("Enter an amount to bet: ");
+            bet = scan.nextInt();
+         }
+         
+      // Initialize Double Down Variable to Twice Bet Amount
+         doubleBet =  2 * bet;
+         
+      // Initial hand for player and dealer
+         Me.playerBet(bet);
+         System.out.println("\tYou have a hand of " + Me.getHandSum());
+         System.out.println("\tDealer is showing " + Dealer.getHandSum());
+         
+      // Dealer Hole Card
+         Dealer.playerHit();
+      
+      // Loop for player hit or stay
+         do {
+            Player.deckCheck();
+            System.out.print("Press 1 to hit, 2 to stay, 3 to double down: ");
+            userInput = scan.nextInt();
+            
+         
+            switch (userInput) {
+               // Player Hit
+               case 1:
+                  Me.playerHit(); 
+                  System.out.println("\tYou have a hand of " + Me.getHandSum());
+                  break;
+               // Player Stay
+               case 2:
+                  break;
+               // Double Down
+               case 3: 
+                  Me.playerBetClear();
+                  if (doubleBet <= Me.getPocket()) {
+                     Me.playerBet(doubleBet);
+                     Me.playerHit();
+                     System.out.println("\tYou have a hand of " + Me.getHandSum());
+                     userInput = 2;
+                     break;
+                  }
+                  if (doubleBet > Me.getPocket() || doubleBet < 0) {
+                     System.out.println("Insufficient Bankroll");
+                     Me.playerBet(bet);
+                     break;
+                  }
+               // Split
+               case 4:
+                  Me.playerBetClear();
+                  if (doubleBet <= Me.getPocket()) {
+                     Me.playerBet(doubleBet);
+                  
+                  }
+                  if (doubleBet > Me.getPocket() || doubleBet < 0) {
+                     System.out.println("Insufficient Bankroll");
+                     break;
+                  }
+               // Incorrect Input
+               default:
+                  System.out.println("Incorrect Input");
+                  break;     
+            }
+         } while (userInput != 2);
+      
+      // Dealer intelligence (to build out)
+         while (Dealer.getHandSum() < 17) {
+            Dealer.playerHit();
+         }
+         System.out.println("\tDealer has a hand of " + Dealer.getHandSum());
+      
+      // Player bust
+         if (Me.getHandSum() > 21 && Me.getWin() == 0) {
+            System.out.println("You Bust");
+            System.out.println("You Lost");
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+      
+      // Player blackjack
+         if (Me.getHandSum() == 21 && Dealer.getHandSum() != 21 
+         && Me.getWin() == 0) {
+            System.out.println("BLACKJACK");
+            Me.blackjack();
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+         
+      // Dealer bust
+         if (Dealer.getHandSum() > 21 && Me.getWin() == 0) {
+            System.out.println("Dealer Bust");
+            System.out.println("You Won");
+            Me.winPot();
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+         
+      // Dealer blackjack
+         if (Dealer.getHandSum() == 21 && Me.getHandSum() != 21 
+         && Me.getWin() == 0) {
+            System.out.println ("DEALER BLACKJACK");
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+         
+      // Player wins Dealer   
+         if (Me.getHandSum() <= 21 && Dealer.getHandSum() <= 21 
+         && Me.getHandSum() > Dealer.getHandSum() && Me.getWin() == 0) {
+            System.out.println("You Won");
+            Me.winPot();
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+         
+      // Dealer beats player
+         if (Me.getHandSum() <= 21 && Dealer.getHandSum() <= 21 
+         && Me.getHandSum() < Dealer.getHandSum() && Me.getWin() == 0) {
+            System.out.println("You Lost");
+            Me.setWin(1);
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+         
+      // Push
+         if (Me.getHandSum() <= 21 && Dealer.getHandSum() <= 21 
+         && Me.getHandSum() == Dealer.getHandSum() && Me.getWin() == 0) {
+            System.out.println("Push");
+            Me.setWin(1);
+            Me.push();
+            Me.clearCards();
+            Dealer.clearCards();
+            System.out.println("You currently have " + Me.getPocket());
+         }
+            
+      // Clears win for next game
+         Me.clearWin();
+         Me.deckCheck();
+         
+      } while (Me.getPocket() != 0);
+      
+      
+      
+      System.out.println("You Lost Your Bankroll \nGame Over!");
+   } 
+}
---
+++ Player.java
@@ -0,0 +1,180 @@
+
+public class Player {
+
+   private static final int MAX = 13;
+   private int card, cardValue = 0, cardValue1 = 0, hitNumber = 0, 
+      win = 0, cardValueSplit = 0;
+   private static int numberOfCards = 312;
+   private static int[] randomCards = new int[312];
+   private static int[] possibleCards = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+   private static int pot = 0, wager = 0;
+   private double pocket, money;
+   
+   public int winToken = 0;
+   
+ // Constructor
+ /** Initializes a player with bankroll of 1000. */
+   public Player() {
+      pocket = 1000;
+   }
+   
+  // Methods
+  /** Sets winValue to 1 to conclude card match up.
+  * @param winValue to 1 for conclusion, 0 for inconclusive
+  */
+   public void setWin(int winValue) {
+      win = winValue;
+   }
+   
+  /** Returns an integer for winValue.
+  * @return int for win
+  */
+   public int getWin() {
+      return win;
+   }
+   
+   /** Sets winValue to 0.
+   */
+   public void clearWin() {
+      win = 0;
+   }
+	
+   /** Returns sum of cards in player's hand.
+   * @return int for cardValue
+   */
+   public int getHandSum() {
+      return cardValue;
+   }
+   
+   /** Returns number of cards in player's hand.
+   * @return int for hitNumber
+   */
+   public int numberOfCards() {
+      return hitNumber;
+   }
+	
+   /** Returns a random card from ace to king and adds to hand.
+   * @return int for cardValue
+   */
+   public int playerHit() {
+      int i = ((int) (Math.random() * numberOfCards));
+      int randomCard = randomCards[i];
+      
+      for (int j = i; j < randomCards.length - 1; j++) {
+         randomCards[j] = randomCards[j + 1];
+      }
+      randomCards[numberOfCards - 1] = 0;
+      numberOfCards--;
+      
+      if (randomCard == 1 && cardValue < 11) {
+         randomCard = 11;
+         cardValue += randomCard;
+      }
+      
+      else if (randomCard == 0) {
+         do {
+            i = ((int) (Math.random() * numberOfCards));
+            randomCard = randomCards[i];
+         } while (randomCard == 0);
+         cardValue += randomCard;
+      }
+      
+      else {
+         cardValue += randomCard;
+      }
+      hitNumber++;
+      return randomCard;
+   }
+
+	
+   /** Places a bet.
+   * @param wager for bet amount
+   */
+   public void playerBet(int wager) {
+      pot = wager * 2;
+      pocket -= wager;
+   }
+   
+   /** Clears the current bet to 0.
+   */
+   public void playerBetClear() {
+      wager = pot / 2;
+      pocket += wager;
+   }
+   
+   /** Conclusion: both players' hands equal.
+   * Returns bet to player' pocket
+   */
+   public void push() {
+      pocket += pot / 2;
+   }
+   
+   /** Returns money in Player's pocket.
+   * @return double for pocket
+   */
+   public double getPocket() {
+      return pocket;
+   }
+   
+   /** Sets pocket to amount.
+   * @param money
+   */
+   public void setPocket(double money) {
+      pocket = money;
+   }
+   
+   /** Adds amount to pocket.
+   * @param money 
+   */
+   public void addPocket(double money) {
+      pocket += money;
+   }
+   
+   /** Player wins pot.
+   */
+   public void winPot() {
+      pocket += pot;
+   }
+   
+   /** Conclusion: Player hits blackjack.
+   */
+   public void blackjack() {
+      pocket += pot * 1.25;
+   }
+   
+   /** Clears players hand to 0.
+   */
+   public void clearCards() {
+      cardValue = 0;
+   }
+   
+   /** Returns number of cards.
+   */
+   public int deckNumber() {
+      return numberOfCards;
+   }
+   
+   /** Reshuffles the deck
+   * @return String if shuffled
+   */
+   public static String deckCheck() {
+      String output = "";
+      int i = 0;
+      if (numberOfCards <= 78) {
+         resetDeck();
+         output = "---------- Deck was Shuffled ----------";
+      }
+      return output;   
+   }
+   
+   /** Sets deck back to 6-deck
+   */
+   public static void resetDeck() {
+      for (int i = 0; i < randomCards.length; i++) {
+         int j = i % 13;
+         randomCards[i] = possibleCards[j];
+      }
+   }
+   
+  
+}
